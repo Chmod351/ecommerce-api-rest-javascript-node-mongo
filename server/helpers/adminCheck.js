8d9ad60cdead7c9c30ac7c1b858c31167f43048f
yamil351 <leokasper@live.com.ar>
2023-05-26 11:11:54 -0300

    fixing misspell in admin midleware

--- server/helpers/adminCheck.js
+++ server/helpers/adminCheck.js
@@ -2,10 +2,11 @@ import User from '../users/userModel.js';
 
 async function adminCheck(req, res, next) {
   const user = await User.findById(req.user.id);
+  console.log(user);
   if (user && user.isAdmin) {
     next();
   } else {
-    next(new Error('Unauthorized'));
+    next(new Error('unauthorized'));
   }
 }
 
