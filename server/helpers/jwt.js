71e66f9bae90c9e0f266be5b61d22fc8f57deb8d
yamil351 <leokasper@live.com.ar>
2023-05-28 16:10:10 -0300

    product tests

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -4,13 +4,13 @@ import { JWT_TOKEN } from '../../index.js';
 const jwt= new Token()
 
 function authMiddleware(req, res, next) {
-  const authHeader = req.cookies.token;
-  if (!authHeader) {
+  const authCookie = req.cookies.token;
+  if (!authCookie) {
     return res.status(401).json({
       message: 'Unauthorized: Missing or invalid authorization header',
     });
   }
-  const token = authHeader;
+  const token = authCookie;
   try {
     const decoded = jwt.verifyToken(token, JWT_TOKEN);
     req.user = decoded;
---
+++ server/tests/product.test.js
@@ -0,0 +1,78 @@
+const request = require('supertest');
+const { expect } = require('chai');
+const myRequest = request('http://localhost:4000/api');
+
+let jwtToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzI4MDZkOTZlNTQwZTZkMTQ1YzU5MyIsImlhdCI6MTY4NTI5NzM5NCwiZXhwIjoxNjg1NDcwMTk0fQ.uVb288Jbcsyls4D820h0H58IjEjmlxOaxDGOec31ZRQ';
+
+describe('product endpoint', () => {
+  it('should return a list of products', async () => {
+    const res = await myRequest.get('/products');
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body.length).to.be.at.least(1);
+  });
+  it('should return a product by ID', async () => {
+    const productId = '6470ec095e0b0295a98cb0db';
+    const res = await myRequest.get(`/products/${productId}`);
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.have.all.keys(
+      '__v',
+      '_id',
+      'createdAt',
+      'description',
+      'hide',
+      'hot',
+      'name',
+      'price',
+      'tags',
+      'updatedAt',
+    );
+  });
+  it('should return products with a specific tag', async () => {
+    const category = 'elegante';
+    const res = await myRequest.get(`/products/tags?tag=${category}`);
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body.length).to.be.at.least(1);
+  });
+  it('should search for products with a specific query', async () => {
+    const query = '';
+    const res = await myRequest.get('/products/search').query({ q: query });
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body.length).to.be.at.least(1);
+  });
+});
+
+describe('unauthorized user', () => {
+  it('should return 401 unauthorized', async () => {
+    const productId = '60b4c47e5f5e5a001f5e5d6e';
+    const res = await myRequest.put(`/products/hide/${productId}`);
+    expect(res.statusCode).to.equal(401);
+  });
+  it('should return 401 unauthorized', async () => {
+    const productId = '60b4c47e5f5e5a001f5e5d6e';
+    const updatedProduct = {
+      price: 200,
+      hot: false,
+      description: 'This is an updated product.. in English.',
+    };
+    const res = await myRequest
+      .put(`/products/update/${productId}`)
+      .send(updatedProduct);
+    expect(res.statusCode).to.equal(401);
+  });
+  it('should create a new product', async () => {
+    const newProduct = {
+      name: 'New Product',
+      img: 'https://example.com/new-product.jpg',
+      price: 100,
+      description: 'This is a new product.',
+      tags: ['elegante'],
+      hot: true,
+    };
+    const res = await myRequest.post('/products').send(newProduct);
+    expect(res.statusCode).to.equal(401);
+  });
+});
